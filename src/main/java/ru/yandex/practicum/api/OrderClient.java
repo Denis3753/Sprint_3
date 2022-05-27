56f520d83652d60c4e116ab9affe8e2eb8c9d8f8
Denis Kolesnikov <denis3753@yandex.ru>
2022-05-28 00:10:48 +0300

    removed unnecessary requests

--- src/main/java/ru/yandex/practicum/api/OrderClient.java
+++ src/main/java/ru/yandex/practicum/api/OrderClient.java
@@ -21,4 +21,53 @@ public class OrderClient extends ScooterConfig {
                 .log().all();
     }
 
+
+    @Step("Send GET request to /api/v1/orders/track?t={trackNumber}")
+    public ValidatableResponse getOrderByTrackNumber(Integer trackNumber) {
+        return given()
+                .spec(getBaseSpec())
+                .queryParam("t", trackNumber)
+                .log().all()
+                .get(EndPoints.ORDER_PATH + "track")
+                .then()
+                .log().all();
+    }
+
+    @Step("Send GET request to /api/v1/orders?{courierId}")
+    public ValidatableResponse getAllOrders(Integer courierId) {
+        return given()
+                .spec(getBaseSpec())
+                .queryParam("courierId", courierId)
+                .log().all()
+                .get(EndPoints.ORDER_PATH)
+                .then()
+                .log().all();
+    }
+
+
+    @Step("Send PUT request to /api/v1/orders/accept/{orderId}?courierId={courierId}")
+    private ValidatableResponse acceptByOrderIdAndCourierId(Integer orderId, Integer courierId) {
+        return given()
+                .spec(getBaseSpec())
+                .queryParam("courierId", courierId)
+                .log().all()
+                .put(EndPoints.ORDER_PATH + "accept/" + orderId)
+                .then()
+                .log().all();
+    }
+
+    @Step("Send PUT request to /api/v1/orders/accept/?courierId={courierId}")
+    private ValidatableResponse acceptByCourierId(Integer courierId) {
+        return given()
+                .spec(getBaseSpec())
+                .log().all()
+                .put(EndPoints.ORDER_PATH + "accept/" + courierId)
+                .then()
+                .log().all();
+    }
+
+    public ValidatableResponse acceptOrder(Integer orderId, Integer courierId) {
+        return orderId != null ? acceptByOrderIdAndCourierId(orderId, courierId) : acceptByCourierId(courierId);
+    }
+
 }
